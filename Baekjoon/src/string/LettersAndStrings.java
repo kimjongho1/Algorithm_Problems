eb176b14d0b32592ad58383c1ec74432bd665934
bellho <134384518+bellho@users.noreply.github.com>
2023-11-09 13:50:33 +0900

    LettersAndStrings
    - 첫 번째 문제

---
+++ Baekjoon/src/string/LettersAndStrings.java
@@ -0,0 +1,18 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LettersAndStrings {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		int num = Integer.parseInt(br.readLine()) - 1;
+		
+		br.close();
+		
+		System.out.println(str.charAt(num));
+	}
+}
