2fc302ec53d3475280157a60566a5d006cd8d415
bellho <1313kjh@gmail.com>
2023-11-10 05:58:25 +0900

    NumberOfWords
    - 영어 대소문자와 공백으로 이루어진 문자열이 주어진다. 이 문자열에는 몇 개의 단어가 있을까? 이를 구하는 프로그램을 작성하시오. 단, 한 단어가 여러 번 등장하면 등장한 횟수만큼 모두 세어야 한다.

---
+++ Baekjoon/src/string/NumberOfWords.java
@@ -0,0 +1,17 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NumberOfWords {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer str = new StringTokenizer(br.readLine()," ");
+		br.close();
+		
+		System.out.println(str.countTokens());
+	}
+}
