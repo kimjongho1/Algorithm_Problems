538fc7d1c2ab3b662315ef8fa50fb2d5066bb9c9
bellho <134384518+bellho@users.noreply.github.com>
2023-11-11 12:54:22 +0900

    StringRepetition

---
+++ Baekjoon/src/string/StringRepetition.java
@@ -0,0 +1,25 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StringRepetition {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		String[] strArr1 = new String[T];
+		
+		for(int i = 0; i < T; i++) {
+			int R = Integer.parseInt(br.readLine());
+			String str = br.readLine();
+			for(int k = 0; k < R; k++) {
+				String temp = str.substring(k, str.length() - (str.length() - k - 1));
+				
+			}
+		}
+	}
+
+}
