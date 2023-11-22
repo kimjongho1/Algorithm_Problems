056722cdb8cc773ae3e8df4c05c3b67dcc6140f1
bellho <134384518+bellho@users.noreply.github.com>
2023-11-22 19:07:31 +0900

    Update CenterShiftAlgorithm.java

--- Baekjoon/src/generalmath1/CenterShiftAlgorithm.java
+++ Baekjoon/src/generalmath1/CenterShiftAlgorithm.java
@@ -5,20 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class CenterShiftAlgorithm {
+	 public static void main(String[] args) throws IOException {
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		System.out.println(A(n));
-	}
-	public static int A(double n) {
-		double result = 0;
-		if (n != 0) {
-			n = Math.pow(2, n - 1) + n;
-			result = Math.pow(2 , n);
-		} else {
-			result = 0;
-		}
-		return (int)result;
-	}
+	        int n = Integer.parseInt(br.readLine());
+			br.close();
+			
+	        System.out.println((int)Math.pow(Math.pow(2, n) +1, 2));
+	    }
 }
