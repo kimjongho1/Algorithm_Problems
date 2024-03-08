f59a55f3e373a21987efff7b353fb13d15f445f1
bellho <1313kjh@gmail.com>
2024-03-08 17:44:22 +0900

    Update P1253_좋은수.java
    투포인트를 이용한 코드

--- Baekjoon/src/doit/chap03/P1253_좋은수.java
+++ Baekjoon/src/doit/chap03/P1253_좋은수.java
@@ -3,45 +3,41 @@ package doit.chap03;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class P1253_좋은수 {
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		
+		int A[] = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			A[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(arr);
-
+		Arrays.sort(A);
 		int result = 0;
 		for(int k = 0; k < N; k++) {
-			int num = arr[k];
 			int i = 0;
 			int j = N - 1;
 			while(i < j) {
-				if (arr[i] + arr[j] == num) {
-					if(i != num && j != k) {
+				if(A[i] + A[j] == A[k]) {
+					if(i != k && j != k) {
 						result++;
 						break;
-					} else if(i == k) {
+					} else if(i == k){
 						i++;
 					} else {
 						j--;
 					}
-				} else if (arr[i] + arr[j] < num) {
+				} else if(A[i] + A[j] < A[k]) {
 					i++;
 				} else {
 					j--;
 				}
 			}
+			
 		}
 		System.out.println(result);
 		br.close();
