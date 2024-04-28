8fa7752b1198d8e8832f70ea05434e3653102945
bellho <134384518+kimjongho1@users.noreply.github.com>
2024-04-28 13:43:07 +0900

    Update P1026_보물.java
    - 10분 소요
    - Collections.reverseOrder() 메소드를 사용하면 간단한 문제

--- Baekjoon/src/baekjoon/silver/P1026_보물.java
+++ Baekjoon/src/baekjoon/silver/P1026_보물.java
@@ -1,10 +1,33 @@
 package baekjoon.silver;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class P1026_보물 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int S = sc.nextInt();
+		int result = 0;
+		
+		int A[] = new int[S];
+		Integer B[] = new Integer[S];
+		
+		for(int i = 0; i < S; i++) {
+			A[i] = sc.nextInt();
+		}
+		for(int i = 0; i < S; i++) {
+			B[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(A);
+		Arrays.sort(B, Collections.reverseOrder());
+		
+		for(int i = 0; i < S; i++) {
+			result = result + A[i] * B[i];
+		}
+		System.out.println(result);
 	}
 
 }
