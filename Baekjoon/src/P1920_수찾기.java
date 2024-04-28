7738b733bcc6c87b297af996d963d42be990947c
bellho <134384518+kimjongho1@users.noreply.github.com>
2024-04-28 19:25:31 +0900

    Update P1920_수찾기.java
    - 정답을 보고 풀었음
    - 이분 탐색(Binary Search)메소드를 추가하여 해결(쿼드 트리, 색종이 만들기 문제에 사용)

--- Baekjoon/src/P1920_수찾기.java
+++ Baekjoon/src/P1920_수찾기.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P1920_수찾기 {
@@ -7,28 +8,59 @@ public class P1920_수찾기 {
 		
 		int n = sc.nextInt();
 		int nArr[] = new int[n];
-		int count = 0;
 		
 		for(int i = 0; i < n; i++) {
 			nArr[i] = sc.nextInt();
 		}
 		
+		// 배열은 반드시 정렬되어있어야한다.
+		Arrays.sort(nArr);
+		
 		int m = sc.nextInt();
-		int mArr[] = new int[n];
 		
-		for(int i = 0; i < n; i++) {
-			mArr[i] = sc.nextInt();
-		}
-		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < m; i++) {
-			for(int j = 0; j < n; j++) {
-				if(mArr[i] == nArr[j]) {
-					count++;
-				}
-				
-				}
-				System.out.println(count);
-				count = 0;
+			
+			// 찾고자 하는 값이 있을 경우 1, 없을 경우 0을 출력한다.
+			if(binarySearch(nArr, sc.nextInt()) >= 0) {
+				sb.append(1).append('\n');
+			} else {
+				sb.append(0).append('\n');
+			}
 		}
+		System.out.println(sb);
+	}
+	
+	/**
+	 * @param arr 정렬 된 배열
+	 * @param key 찾으려는 값
+	 * @return key와 일치하는 배열의 인덱스
+	 */
+	
+	public static int binarySearch(int[] arr, int key) {
+		
+		int lo = 0;					// 탐색 범위의 왼쪽 끝 인덱스
+		int hi = arr.length - 1;	// 탐색 범위의 오른쪽 끝 인덱스
+		
+		// lo가 hi보다 커지기 전까지 반복한다.
+		while(lo <= hi) {
+			
+			int mid = (lo + hi) / 2;	// 중간위치를 구한다.
+			
+			// key값이 중간 위치의 값보다 작을 경우
+			if(key < arr[mid]) {
+				hi = mid - 1;
+			}
+			// key값이 중간 위치의 값보다 클 경우
+			else if(key > arr[mid]) {
+				lo = mid + 1;
+			}
+			// key값과 중간 위치의 값이 같을 경우
+			else {
+				return mid;
+			}
+		}
+		// 찾고자 하는 값이 존재하지 않을 경우
+		return -1;
 	}
 }
