d28b302ffcde5d47a4cdfe7dd48850b3a4b8f252
bellho <134384518+kimjongho1@users.noreply.github.com>
2024-04-28 14:46:01 +0900

    Create P1065_한수.java
    - 40분 소요
    - line 18 ~ line 20을 좀 더 간결하게 처리하는 방법이 필요해 보인다
    - 좀 더 간단하게 십의 자리, 백의 자리, 천의 자리를 변수 처리 하였으면 간단했을
    문제

---
+++ Baekjoon/src/baekjoon/silver/P1065_한수.java
@@ -0,0 +1,34 @@
+package baekjoon.silver;
+
+import java.util.Scanner;
+
+public class P1065_한수 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String sN = sc.nextLine();
+		int n = Integer.parseInt(sN);
+		int result = 0;
+		int arr[] = new int[3];
+		String stemp = null;
+		
+		if(!(n < 100) && n != 1000) {
+			result = 99;
+			for(int i = 100; i <= n; i++) {
+				for(int j = 0; j < sN.length(); j++) {
+					stemp = Integer.toString(i);
+					arr[j] = stemp.charAt(j) - '0';
+				}
+				if(arr[0] - arr[1] == arr[1] - arr[2]) {
+					result++;
+				}
+			}
+		} else if (n != 1000) {
+			result = n;
+		} else {
+			result = 144;
+		}
+		System.out.println(result);
+	}
+}
