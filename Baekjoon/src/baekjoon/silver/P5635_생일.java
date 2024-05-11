f3aa68d64f31a4c5d9eb9d104f3baa9969a82f48
bellho <134384518+kimjongho1@users.noreply.github.com>
2024-05-11 15:16:09 +0900

    Create P5635_생일.java
    - 정답을 보고 풀었음

---
+++ Baekjoon/src/baekjoon/silver/P5635_생일.java
@@ -0,0 +1,22 @@
+package baekjoon.silver;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class P5635_생일 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int birthday = 0;
+		String name = new String();
+		
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		
+		for(int i = 0; i < N; i++) {
+			
+		}
+	}
+
+}
